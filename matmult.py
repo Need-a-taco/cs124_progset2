42570430fdc223c6c284e2f8f9a015ffa60a58ec
Josh <joshua.zhang@gmail.com>
2024-03-15 01:54:57 -0400

    more matrix

--- matmult.py
+++ matmult.py
@@ -5,6 +5,8 @@ import numpy as np
 
 A = [[1, 2], [3, 4]]
 B = [[5, 6], [7, 8]]
+C = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
+D = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
 u = [3, 4, 5]
 v = [3, 4, 5]
 s = [[3], [4], [5]]
@@ -39,4 +41,39 @@ def matmult(mat1, mat2):
         new_mat.append(new_row)
     return new_mat
 
-print(matmult(A, B))
+
+def strassen_matmult(mat1, mat2):
+    # Meant for square matrices
+    n = len(mat1)
+    upleft_block1 = []
+    upright_block1 = []
+    downleft_block1 = []
+    downright_block1 = []
+    upleft_block2 = []
+    upright_block2 = []
+    downleft_block2 = []
+    downright_block2 = []
+    # This is wrong, needs to make new rows...
+    for i in range(n):
+        for j in range(n):
+            if (i < n/2) and (j < n/2):
+                upleft_block1.append(mat1[i][j])
+            elif (i < n/2) and (j > n/2):
+                upright_block1.append(mat1[i][j])
+            elif (i > n/2) and (j < n/2):
+                downleft_block1.append(mat1[i][j])
+            else:
+                downright_block1.append(mat1[i][j])
+    for i in range(n):
+        for j in range(n):
+            if (i < n/2) and (j < n/2):
+                upleft_block2.append(mat2[i][j])
+            elif (i < n/2) and (j > n/2):
+                upright_block2.append(mat2[i][j])
+            elif (i > n/2) and (j < n/2):
+                downleft_block2.append(mat2[i][j])
+            else:
+                downright_block2.append(mat2[i][j])
+    print(upleft_block1, upleft_block2)
+    
+print(strassen_matmult(C, D))
