52016654317696158216adcb3285b8951714e4ad
PerdoGarcia <pedro_garcia@college.harvard.edu>
2024-03-27 00:03:02 -0400

    alot of stuff done

--- strassen.py
+++ strassen.py
@@ -29,10 +29,8 @@ def mergeblocks(upleft, upright, downleft, downright):
     return new_mat
 
 def pad_matrix(mat):
-    original_size = len(mat) 
-    next_power_of_2 = 2**np.ceil(np.log2(original_size)).astype(int)
-    pad_len = next_power_of_2 - original_size
-    pad_np_mat = np.pad(mat, ((0, pad_len), (0, pad_len)), 'constant', constant_values=0)
+    np_mat = np.array(mat)
+    pad_np_mat = np.pad(np_mat, ((0, 1), (0, 1)), mode='constant', constant_values=0)
     pad_mat = pad_np_mat.tolist()
     return pad_mat
 
@@ -76,13 +74,12 @@ def conventional_matmult(mat1, mat2):
         new_mat.append(new_row)
     return new_mat
 
-def strassen_matmult(mat1, mat2):
-    # Make dimensions a power of 2
-    if len(mat1) > 0 and np.log2(len(mat1)) % 1 != 0:
+def strassen_matmult(mat1, mat2, threshold):
+    if len(mat1) % 2 == 1:
         mat1 = pad_matrix(mat1)
         mat2 = pad_matrix(mat2)
     # Establish an arbitrary base case. To be adjusted
-    if (len(mat1) <= 2):
+    if (len(mat1) <= threshold):
         mat_prod = conventional_matmult(mat1, mat2)
         return mat_prod
         
@@ -107,13 +104,13 @@ def strassen_matmult(mat1, mat2):
 
     
     # --- Calculate multiplication with only 7 variables --- # 
-    p1 = strassen_matmult(a, matrix_subtraction(f, h))
-    p2 = strassen_matmult(matrix_addition(a, b), h)
-    p3 = strassen_matmult(matrix_addition(c, d), e)
-    p4 = strassen_matmult(d, matrix_subtraction(g, e))
-    p5 = strassen_matmult(matrix_addition(a, d), matrix_addition(e, h))
-    p6 = strassen_matmult(matrix_subtraction(b, d), matrix_addition(g, h))
-    p7 = strassen_matmult(matrix_subtraction(a, c), matrix_addition(e, f))
+    p1 = strassen_matmult(a, matrix_subtraction(f, h), threshold)
+    p2 = strassen_matmult(matrix_addition(a, b), h, threshold)
+    p3 = strassen_matmult(matrix_addition(c, d), e, threshold)
+    p4 = strassen_matmult(d, matrix_subtraction(g, e), threshold)
+    p5 = strassen_matmult(matrix_addition(a, d), matrix_addition(e, h), threshold)
+    p6 = strassen_matmult(matrix_subtraction(b, d), matrix_addition(g, h), threshold)
+    p7 = strassen_matmult(matrix_subtraction(a, c), matrix_addition(e, f), threshold)
 
     # --- Calculate the new blocks with these 7 variables --- #
     
@@ -137,45 +134,34 @@ def strassen_matmult(mat1, mat2):
 
 # Task 1
 def testing_threshold():
-    dimension = sys.argv[2]
-    strassen_avg_runtime = 0
-    conventional_avg_runtime = 0
+    dimension = 256
+    threshold = 8
+    stras_list = []
+    conv_list = []
+    for dim in range(2, dimension):
+        matrix =[[random.randint(0, 20) for _ in range(dim)] for _ in range(dim)]
+        #strassens
+        stras_avg_runtime = 0
+        for _ in range(5):
+            start = time.time()
+            matmult = strassen_matmult(matrix, matrix, dim-1)
+            matmult = [row[:dim] for row in matmult[:dim]]
+            end = time.time()
+            stras_avg_runtime += (end - start)
+        stras_avg_runtime /= 5
+        stras_list.append(stras_avg_runtime)
+        
+        #conventional
+        conv_avg_runtime = 0
+        for _ in range(5):
+            start = time.time()
+            matmult = conventional_matmult(matrix, matrix)
+            end = time.time()
+            conv_avg_runtime += (end - start)
+        conv_avg_runtime /= 5
+        conv_list.append(conv_avg_runtime)
+        
     
-    for _ in range(5):
-        matrix =[[random.randint(0, 2) for _ in range(dimension)] for _ in range(dimension)]
-        
-        mat1 = matrix
-        mat2 = matrix
-        
-        # Timed part
-        start = time.time()
-        matmult = strassen_matmult(mat1, mat2)
-        if len(matmult) != dimension:
-            matmult = [row[:dimension] for row in matmult[:dimension]]
-        end = time.time()
-        
-        strassen_avg_runtime += (end - start)
-    strassen_avg_runtime = strassen_avg_runtime / 5
-    
-    for _ in range(5):
-        matrix =[[random.randint(0, 100) for _ in range(dimension)] for _ in range(dimension)]
-        
-        mat1 = matrix
-        mat2 = matrix
-        
-        # Timed part
-        start = time.time()
-        matmult = strassen_matmult(mat1, mat2)
-        if len(matmult) != dimension:
-            matmult = [row[:dimension] for row in matmult[:dimension]]
-        end = time.time()
-        
-        conventional_avg_runtime += (end - start)
-    conventional_avg_runtime = conventional_avg_runtime / 5
-    print(f"Strassen runtime: {strassen_avg_runtime} \n")
-    print(f"Conventional runtime: {strassen_avg_runtime}")
-        
-
 # Task 2: What we actually submit to Gradescope
 def matrix_multplication():
     # Parse input file
@@ -194,10 +180,10 @@ def matrix_multplication():
     for i in range((entries_len // 2), entries_len, dimension):
         mat2.append(entries[i : i + dimension])
     
-    matmult = strassen_matmult(mat1, mat2)
+    matmult = strassen_matmult(mat1, mat2, dimension-1)
     if len(matmult) != dimension:
         matmult = [row[:dimension] for row in matmult[:dimension]]
-    
+        
     for i in range(dimension):
         print(matmult[i][i])
 
@@ -212,7 +198,7 @@ def count_triangles(n, p):
             if i != j:
                 adj_mat[j][i] = val
     # Find the cube of adjacency matrix
-    adj_mat_cubed = strassen_matmult(adj_mat, strassen_matmult(adj_mat, adj_mat))
+    adj_mat_cubed = strassen_matmult(adj_mat, strassen_matmult(adj_mat, adj_mat, 50), 50)
     print("\n")
     print(adj_mat_cubed)
     
@@ -224,7 +210,7 @@ def count_triangles(n, p):
     
         
 def main():
-    count_triangles(1024, 0.01)
+    matrix_multplication()
 
 if __name__ == "__main__":
     main()
